1a909ac2e7ffda10153fa53100c1eeac829e57a2
kirtikaj27 <kirtika.jaiswal1996@gmail.com>
2020-05-03 10:33:29 +0530

    Last Update

--- request.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-resp = requests.post("http://localhost:5000/predict",
-                     files={"file": open('C:/Users/APURAV/Desktop/project-dog-classification/sample_images/beaglier.jpg','rb')})
